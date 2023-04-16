8ff09468feefc1489cdf80e516e0acb487165bc0
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-04-16 23:18:28 +0530

    Update bubble.c

--- bubble.c
+++ bubble.c
@@ -6,26 +6,23 @@
 
 void bubble(int n, int * a)
 {
-   int i,j,temp ,count;
+   int i,j,temp ,count=0;
     for(i=0;i<n-1;i++)
     {
+         count=0;
         for(j=0;j<n-i-1;j++)
         {
-            count=0;
             if(*(a+j)>*(a+j+1))
             {
-                count = 1;
+                count++;
                 temp=*(a+j);
                 *(a+j)=*(a+j+1);
                 *(a+j+1)=temp;
             }
         }
-        if(count!=0)
+        if(count==0)
         break;
     }
-
-
-
 }
 
 void main()

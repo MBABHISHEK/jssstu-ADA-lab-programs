761d073234743327112d574fc731ee5c0b768364
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-04-16 22:50:20 +0530

    Add files via upload
    Analysis of Bubble sort with worst case

---
+++ bubble.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+
+
+void bubble(int n, int * a)
+{
+   int i,j,temp ,count;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            count=0;
+            if(*(a+j)>*(a+j+1))
+            {
+                count = 1;
+                temp=*(a+j);
+                *(a+j)=*(a+j+1);
+                *(a+j+1)=temp;
+            }
+        }
+        if(count!=0)
+        break;
+    }
+
+
+
+}
+
+void main()
+{
+      clock_t st,et;
+
+     FILE *d,*g;
+     d=fopen("data.txt","a");
+     g=fopen("graph.txt","a");
+     int n=10;
+     int k,j;
+     for(k=0;k<5;k++)
+     {
+    int * a=(int *)malloc(sizeof(int)*n);
+     for(int i=0;i<n;i++)
+      {  *(a+i)=n-i;
+          fprintf(d,"%d ",*(a+i));
+      }
+        fprintf(d,"\n\n\n");
+        st=clock();
+
+        bubble(n,a);
+
+        et=clock();
+
+        double result=(et-st)/CLOCKS_PER_SEC;
+        printf(" the %d is %lf\n", n,result);
+        fprintf(g,"%d    %lf\n",n,result);
+        n=n*10;
+    
+
+     
+
+     }
+
+
+
+    fclose(d);
+    fclose(g);
+
+
+}
+
+/*
+void bubble(int n, int * a)
+{
+   int i,j,temp;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(*(a+j)>*(a+j+1))
+            {
+                temp=*(a+j);
+                *(a+j)=*(a+j+1);
+                *(a+j+1)=temp;
+            }
+        }
+    }
+
+
+}*/
